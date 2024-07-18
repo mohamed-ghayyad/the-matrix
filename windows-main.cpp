cde769de6486aeb451e6dd0dbbaa0bd96d692c3b
Mohamed Jamal Ghayyad <mohammed.gamal@live.com>
2024-07-18 04:13:05 +0100

    Update windows-main.cpp

--- windows-main.cpp
+++ windows-main.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<windows.h>
-#include<limits>
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
 
 using namespace std;
- // Some old MinGW/CYGWIN distributions don't define this:
+
+// Some old MinGW/CYGWIN distributions don't define this:
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING  0x0004
 #endif
@@ -32,6 +34,7 @@ void setupConsole(void) {
         exit(GetLastError());
     }   
 }
+
 void restoreConsole(void) {
     // Reset colors
     printf("\x1b[0m");  
@@ -41,30 +44,43 @@ void restoreConsole(void) {
         exit(GetLastError());
     }
 }
-int main(){
+
+int main() {
     int matrixSize = 10;
+    int speed = 100; // Default speed in milliseconds
+    int density = 50; // Default density percentage (0-100)
+
     setupConsole();
-    while(true){
-        std::cout << "Enter The matrix(10000): ";
+    srand(time(0)); // Seed for random number generation
+    
+    while (true) {
+        std::cout << "Enter the matrix size (e.g., 10): ";
         std::cin >> matrixSize;
-        if(matrixSize > 0){
-            for(int i = 0; i < matrixSize; i++){
-                for(int j = 0; j < matrixSize; j++){
-                    if(i%2 == j%2){
+        
+        if (matrixSize > 0) {
+            std::cout << "Enter the delay time in milliseconds (e.g., 100 for 0.1 seconds): ";
+            std::cin >> speed;
+            
+            std::cout << "Enter the density percentage (0-100): ";
+            std::cin >> density;
+            
+            for (int i = 0; i < matrixSize; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    if (rand() % 100 < density) {
                         std::cout << "\033[1;32m1\033[0m ";
-                    }else if(i%3 == j%5){
-                        std::cout << "\033[1;32m1\033[0m ";
-                    }else{
+                    } else {
                         std::cout << "\033[1;32m0\033[0m ";
                     }
                 }
                 std::cout << std::endl;
+                Sleep(speed); // Control the speed by introducing a delay
             }
             break;
-        }else{
+        } else {
             std::cout << "Invalid input. Please enter a positive integer." << std::endl;
         }
     }
+
     restoreConsole();
     return 0;
 }
